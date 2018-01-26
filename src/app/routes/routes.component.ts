f4a613e21a360d35f550ab7d1f32a63ffcde0c55
Oona Österberg <oonaos@ety-a2123b-06.edu.wks.metropolia.fi>
2018-01-26 12:20:58 +0200

    v2t1

---
+++ src/app/routes/routes.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {DigitransitService} from '../services/digitransit.service';
+
+@Component({
+    selector: 'app-routes',
+    templateUrl: './routes.component.html',
+    styleUrls: ['./routes.component.scss'],
+})
+export class RoutesComponent implements OnInit {
+
+    pysakki = 'Gransinmäki';
+
+    constructor(private digitransitService: DigitransitService) {
+    }
+
+    ngOnInit() {
+        this.digitransitService.getRoutes(this.pysakki).subscribe(response => {
+            console.log(response.data['stops']);
+        });
+    }
+
+}
---
+++ src/app/services/media.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class MediaService {
+
+    testi = 'moi vaan';
+    apiUrl = 'http://media.mw.metropolia.fi/wbma';
+
+    constructor(private http: HttpClient) {
+    }
+
+    getAllMedia() {
+        return this.http.get(this.apiUrl + '/media');
+    }
+
+}
